34d41b99058bc6ba1fcb04fef02193050277c719
Bradley Nichol <bradleynichol@live.co.uk>
2020-05-14 22:17:42 +0100

    app initialised

--- shoppingBasket.js
+++ shoppingBasket.js
@@ -3,3 +3,11 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
+
+const app = express();
+
+app.use(cors());
+
+
+
+module.exports.handler = serverless(app);
