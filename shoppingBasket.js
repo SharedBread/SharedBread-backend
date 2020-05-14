860f00d172d7b0c7859d471a980558eb83884d0e
Bradley Nichol <bradleynichol@live.co.uk>
2020-05-14 23:26:42 +0100

    db connection added

--- shoppingBasket.js
+++ shoppingBasket.js
@@ -4,6 +4,14 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: 'sharedbread',
+  connectionLimit: 10,
+});
+
 const app = express();
 
 app.use(cors());
