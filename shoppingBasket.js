62ec5b169c41adbdbed794efffd3019020aaa1d5
Bradley Nichol <bradleynichol@live.co.uk>
2020-05-14 22:15:03 +0100

    imports

---
+++ shoppingBasket.js
@@ -0,0 +1,5 @@
+const serverlessHTTP = require('serverless-http');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
