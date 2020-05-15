81fbe1dd7c8dc624433be74ad849cf0c0da65c0e
Bradley Nichol <bradleynichol@live.co.uk>
2020-05-15 23:21:33 +0100

    userId added to get endpoint

--- userProfileData.js
+++ userProfileData.js
@@ -17,7 +17,10 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/profile', (request, response) => {
+app.get('/profile/:userId', (request, response) => {
+  const { userId } = request.params;
+
+
   db.query('SELECT * FROM UserTable', (err, data) => {
     if (err) {
       console.log('Error from MySQL', err);
