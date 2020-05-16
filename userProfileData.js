71f8364d076a4ba1f7e2c4debc7fed4dd7c9ce14
Bradley Nichol <bradleynichol@live.co.uk>
2020-05-16 14:35:33 +0100

    seperate user check from donations for validation

--- userProfileData.js
+++ userProfileData.js
@@ -23,10 +23,9 @@ app.get("/profile/:userId", (req, res) => {
   // get the new users info
   const newUser = req.body;
 
-  const query = "SELECT * FROM UserTable JOIN FoodDonations ON UserTable.UserID = FoodDonations.ID WHERE AuthId = ?";
-  db.query(query, userId, (err, data) => {
+  const query = "SELECT * FROM UserTable WHERE AuthId = ?";
+  db.query(query, userId, (err) => {
     if (err) {
-      console.log("Error from MySQL", err);
       res.status(500).send(err);
 
     // if no user is found, add the user to the DB
